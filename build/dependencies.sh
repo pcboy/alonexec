ef0916fd60cb21a924a829dce4ee17341d5e282e
pcboy <none@none>
2010-04-26 18:56:37 +0200

    Make sexp urls in dependencies.sh more dynamic.

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -1,14 +1,20 @@
 LASTDIR=`pwd`
 cd `dirname $0`
 
-if [ -d sexpr_1.2/ ];then
+SEXP_VERSION="1.2"
+SEXP_SOURCE="http://downloads.sourceforge.net/project/sexpr/sexpr/$SEXP_VERSION/sexpr_$SEXP_VERSION.tar.gz"
+
+if [ -d sexpr_$SEXP_VERSION/ ];then
     exit
 fi
+
 echo -e "Getting sexpr library\n\n"
-wget http://downloads.sourceforge.net/project/sexpr/sexpr/1.2/sexpr_1.2.tar.gz &&\
-         tar xvf sexpr_1.2.tar.gz && cd sexpr_1.2/ && ./configure && make &&\
-         cd .. && echo -e "\n\nSexpr library successfully built"
-if [ -f sexpr_1.2.tar.gz ];then
-    rm -f sexpr_1.2.tar.gz
+wget $SEXP_SOURCE &&\
+         tar xvf sexpr_$SEXP_VERSION.tar.gz &&\
+         pushd sexpr_$SEXP_VERSION/ && ./configure && make &&\
+         popd && echo -e "\n\nSexpr library successfully built"
+
+if [ -f sexpr_$SEXP_VERSION.tar.gz ];then
+    rm -f sexpr_$SEXP_VERSION.tar.gz
 fi
 cd $LASTDIR
