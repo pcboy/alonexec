61ece6d057f8c9a1fd9e1e83b6d5c26a6debcd4a
pcboy <none@none>
2010-05-16 02:27:42 +0200

    Now dependencies.sh fetches last TCC version.

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -1,20 +1,51 @@
 LASTDIR=`pwd`
 cd `dirname $0`
 
-SEXP_VERSION="1.2"
-SEXP_SOURCE="http://downloads.sourceforge.net/project/sexpr/sexpr/$SEXP_VERSION/sexpr_$SEXP_VERSION.tar.gz"
 
-if [ -d sexpr_$SEXP_VERSION/ ];then
-    exit
-fi
+# Getting sexp library.
 
-echo -e "Getting sexpr library\n\n"
-wget $SEXP_SOURCE &&\
-         tar xvf sexpr_$SEXP_VERSION.tar.gz &&\
-         pushd sexpr_$SEXP_VERSION/ && ./configure && pushd src && make &&\
-         popd && popd && echo -e "\n\nSexpr library successfully built"
+function compile_sexp() {
+    SEXP_VERSION="1.2"
+    SEXP_SOURCE="http://downloads.sourceforge.net/project/sexpr/sexpr/$SEXP_VERSION/sexpr_$SEXP_VERSION.tar.gz"
 
-if [ -f sexpr_$SEXP_VERSION.tar.gz ];then
-    rm -f sexpr_$SEXP_VERSION.tar.gz
-fi
+    if [ -d sexpr_$SEXP_VERSION/ ];then
+        return
+    fi
+
+    echo -e "Getting sexpr library\n\n"
+    wget $SEXP_SOURCE &&\
+             tar xvf sexpr_$SEXP_VERSION.tar.gz &&\
+             pushd sexpr_$SEXP_VERSION/ && ./configure && make &&\
+             popd && echo -e "\n\nSexpr library successfully built"
+
+    if [ -f sexpr_$SEXP_VERSION.tar.gz ];then
+        rm -f sexpr_$SEXP_VERSION.tar.gz
+    fi
+}
+
+
+# Getting TCC.
+
+function compile_TCC() {
+    TCC_VERSION="0.9.25"
+    TCC_SOURCE="http://download.savannah.nongnu.org/releases/tinycc/tcc-$TCC_VERSION.tar.bz2"
+
+    if [ -d tcc-$TCC_VERSION/ ];then
+        return
+    fi
+
+    echo -e "Getting TCC\n\n"
+    wget $TCC_SOURCE &&\
+             tar xvf tcc-$TCC_VERSION.tar.bz2 &&\
+             pushd tcc-$TCC_VERSION/ && ./configure && make && mv tcc ../ &&\
+             popd && echo -e "\n\nTCC successfully built"
+
+    if [ -f tcc_$TCC_VERSION.tar.bz2 ];then
+        rm -f tcc_$TCC_VERSION.tar.bz2
+    fi
+}
+
+compile_sexp
+compile_TCC
+
 cd $LASTDIR
